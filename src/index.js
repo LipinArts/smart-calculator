28b4ea40a056edfdbfed9710e250bd693f3f5709
Artsiom Lipin <lipinartsiom@gmail.com>
2018-03-02 19:23:16 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+	constructor(initialValue) {
+		this.initialValue = initialValue;
+	}
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+	add(number) {
+		this.initialValue = new Function("a, b", "return a + '+' + b")(
+			this.initialValue,
+			number
+		);
+		return this;
+	}
 
-  multiply(number) {
-    // your implementation
-  }
+	subtract(number) {
+		this.initialValue = new Function("a, b", "return a + '-' + b")(
+			this.initialValue,
+			number
+		);
+		return this;
+	}
 
-  devide(number) {
-    // your implementation
-  }
+	multiply(number) {
+		this.initialValue = new Function("a, b", "return a + '*' + b")(
+			this.initialValue,
+			number
+		);
+		return this;
+	}
 
-  pow(number) {
-    // your implementation
-  }
+	devide(number) {
+		this.initialValue = new Function("a, b", "return a + '/' + b")(
+			this.initialValue,
+			number
+		);
+		return this;
+	}
+
+	pow(number) {
+		this.initialValue = new Function("a, b", "return a + '**' + b")(
+			this.initialValue,
+			number
+		);
+		return this;
+	}
+	valueOf() {
+		this.initialValue = new Function("a, b", "return " + this.initialValue)();
+		return this.initialValue;
+	}
 }
 
 module.exports = SmartCalculator;
